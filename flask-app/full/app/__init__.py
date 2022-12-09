b31104f3a8028116700e0853e65bd93ef9382090
parsa <parsa@beastbrain.org>
2022-12-09 16:03:58 +0330

    fixed a bug-added apispec marshmellow and swagger

--- flask-app/full/app/__init__.py
+++ flask-app/full/app/__init__.py
@@ -1,38 +1,11 @@
-# rawww flask template
-# from flask import Flask
-# from flask import jsonify
-
-# # import logging
-# # logger = logging.getLogger(__name__)
-# # logger.addHandler(logging.StreamHandler(stream=sys.stderr))
-# # logger.setLevel(logging.DEBUG)
-
-
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def main():
-
-#     return jsonify({"message": "somestuff"})
-
-
-# if __name__ == "__main__":
-
-#     app.run(debug=True)
-
-#####################################################################################
-# using flask_restful
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
-
-# from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
 from sqlalchemy import select
 import logging
-
-# from api.api import Hello
 import os
+from flask_apispec.extension import FlaskApiSpec
+
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # creating the flask app
@@ -40,6 +13,7 @@ app = Flask(__name__)
 # creating an API object
 apis = Api(app)
 # adding configuration for using a sqlite database
+docs = FlaskApiSpec(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 if __name__ != "__main__":
     gunicorn_logger = logging.getLogger("gunicorn.error")
@@ -47,9 +21,22 @@ if __name__ != "__main__":
     app.logger.setLevel(gunicorn_logger.level)
 from app.models.database import db
 
-# Creating an SQLAlchemy instance
-# db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+
+app.config.update(
+    {
+        "APISPEC_SPEC": APISpec(
+            title="calender",
+            version="v1",
+            plugins=[MarshmallowPlugin()],
+            openapi_version="2.0.0",
+        ),
+        "APISPEC_SWAGGER_URL": "/swagger",  # URI to access API Doc JSON
+        "APISPEC_SWAGGER_UI_URL": "/swagger-ui",  # URI to access UI of API Doc
+    }
+)
 
 
 # another resource to calculate the square of a number
@@ -59,6 +46,7 @@ from app.api.api import Hello
 
 # adding the defined resources along with their corresponding urls
 apis.add_resource(Hello, "/")
+docs.register(Hello)
 
 
 # driver function
--- flask-app/full/app/api/api.py
+++ flask-app/full/app/api/api.py
@@ -1,33 +1,37 @@
-# making a class for a particular resource
-# the get, post methods correspond to get and post requests
-# they are automatically mapped by flask_restful.
-# other methods include put, delete, etc.
-
+from app.models.models import Profile
 from flask_restful import Resource, Api
 from flask import Flask, jsonify, request
-
-# from app.application import db
 from sqlalchemy import select
-from app.models.models import Profile
-
-# from app import api, db
+from app.models.models import Event
 from app.models.database import db
+from flask_apispec import marshal_with, doc, use_kwargs
+from flask_apispec.views import MethodResource
+from marshmallow import Schema, fields
 
 
-class Hello(Resource):
+# class EventSchema(Schema):
+#     message = fields.Str(default="Success")
+
+
+class HelloSchema(Schema):
+    message = fields.Str(required=False)
+    error = fields.Dict(required=False)
+
+
+class Hello(Resource, MethodResource):
+    @doc(description="this is where u can manipulate and get events", tags=["event"])
+    @marshal_with(HelloSchema)
     def get(self):
-        print("hellooooooooooooooooooooooooooo")
-        profile = db.session.execute(select(Profile).filter_by(id=1)).one()[0]
-        print(profile)
-        return jsonify({"messages": profile.first_name, "age": profile.age})
+        try:
+            event = db.session.execute(select(Profile).filter_by(id=1)).one()[0]
+            return {"message": "Hello, World!"}, 200
+        except Exception as e:
+            return {"error": e, "error": {"code": 500, "trace": "some random"}}, 200
 
     # Corresponds to POST request
-    def post(self):
+    @use_kwargs(HelloSchema, location=("json"))
+    @marshal_with(HelloSchema)
+    def post(self, **kwargs):
 
-        data = request.get_json()  # status code
-        return jsonify({"data": data}), 201
-
-
-#
-# api.add_resource(Hello, "/")
-#
+        # data = request.get_json()  # status code
+        return {"message": "sth", "error": {"code": 500, "trace": "sorandom"}}, 200
